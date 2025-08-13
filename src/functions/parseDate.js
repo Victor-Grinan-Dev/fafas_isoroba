214a974be375230935a102d04908d289fd0e9368
victor grinan <victorgrinan@gmail.com>
2025-08-13 16:26:11 +0300

    Remove unused date parsing and time formatting functions

--- src/functions/parseDate.js
+++
@@ -1,6 +0,0 @@
-export const parseDateString = (str) => {
-  const match = str.match(/Date\((\d+),(\d+),(\d+),(\d+),(\d+),(\d+)\)/);
-  if (!match) return str;
-  const [_, year, month, day, hour, minute, second] = match.map(Number);
-  return new Date(year, month, day, hour, minute, second).toISOString();
-}
--- src/functions/parseHours.js
+++
@@ -1,6 +0,0 @@
-export const formatTime = (cell) => {
-  if (cell instanceof Date) {
-    return cell.toTimeString().slice(0,5); // "HH:MM"
-  }
-  return cell;
-}
